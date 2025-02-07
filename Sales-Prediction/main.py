a1aeda44a05a55bebb316446dc560815280ad52d
Temple <127430671+Tdavis23@users.noreply.github.com>
2025-02-07 01:06:37 +0000

    completed the assignment

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,14 +1,15 @@
 #
-# Name
-# Date
+# Trenton B. Davis 
+# 02/06/2005
 # Sales Prediction Programming Project
 # COSC 1010
 #
 
 # Variables to hold the sales total and the profit
-
+salesTotal = float(input("Enter how much you made from the sale: "))
 # Get the amount of projected sales.
 
 # Calculate the projected profit.
-
+predictedProfit = salesTotal * 0.23
 # Print the projected profit.
+print("The profit off the sale was $", "{:,.2f}".format(predictedProfit))
