7438c8db0e85e67ec28c42d262ea8c8767d658ed
Temple <127430671+Tdavis23@users.noreply.github.com>
2025-03-29 02:16:54 +0000

    completed hot dog cookout calculator

--- Hot-Dog-Cookout-Calculator/main.py
+++ Hot-Dog-Cookout-Calculator/main.py
@@ -1,7 +1,38 @@
 #
-# Name
-# Date
+# Trenton B. Davis
+# 03/28/2025
 # Hot Dog Cookout Calculator Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+def HotDogCookoutCalculator():
+    #define global CONSTANTS
+    HOTDOGS_PER_PACKAGE = 10 
+    BUNS_PER_PACKAGE = 8
+
+    #inputs 
+    attendees = int(input("How Many People are going? "))
+    hotDogsPerPerson = int(input("How many hot dogs will each person recieve? "))
+
+    #calculate hotDogs and Buns needed
+    totalHotDogsNeeded = attendees * hotDogsPerPerson
+
+    hotDogPackagesNeeded = (totalHotDogsNeeded + HOTDOGS_PER_PACKAGE -1 ) // HOTDOGS_PER_PACKAGE
+
+    totalBunsNeeded = attendees
+
+    bunPackagesNeeded = (totalBunsNeeded + BUNS_PER_PACKAGE -1 ) // BUNS_PER_PACKAGE
+
+    #calculate leftover buns and hotDogs
+    leftoverHotDogs = hotDogPackagesNeeded * HOTDOGS_PER_PACKAGE - totalHotDogsNeeded
+    leftoverBuns = bunPackagesNeeded * BUNS_PER_PACKAGE - totalBunsNeeded
+
+    #print the information
+    print(f"\nMinimum number of packages of hot dogs required: {hotDogPackagesNeeded}")
+    print(f"Minimum number of packages of hot dog buns required: {bunPackagesNeeded}")
+    print(f"Number of hot dogs that will be left over: {leftoverHotDogs}")
+    print(f"Number of hot dog buns that will be left over: {leftoverBuns}")
+
+#run the function
+HotDogCookoutCalculator()
