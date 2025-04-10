57eff18cdf7a32f77f85ca6bcedeca0df66e4126
Temple <127430671+Tdavis23@users.noreply.github.com>
2025-04-10 23:56:16 +0000

    completed lottery number generator

--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,25 @@
 #
-# Name
+# Trenton B. Davis
 # Date
 # Lottery Number Generator Programming Project
 # COSC 1010
 #
+import random 
+
+def numberGenerator():
+
+    # call var
+    numberList = []
+
+    #define lottery numbers
+    for index in range(7):
+        numberList.append(random.randint(0,9))
+    
+    #print lottery numbers
+    print("did you get the winning ticket?")
+    for index in range(len(numberList)):
+        print(numberList[index], end="" )
+
+        
+numberGenerator()
 # Use comments liberally throughout the program. 
