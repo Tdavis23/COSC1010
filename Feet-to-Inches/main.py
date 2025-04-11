124ffa68821f5402cbc0e600cedd7fef547a52e1
Temple <127430671+Tdavis23@users.noreply.github.com>
2025-04-11 01:14:32 +0000

    completed Feet too inches assignment

--- Feet-to-Inches/main.py
+++ Feet-to-Inches/main.py
@@ -1,7 +1,13 @@
 #
-# Name
-# Date
+# Trenton Davis
+# 04/10/25
 # Feet to Inches Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program.
+def FeetToInches(feet):
+    inches = float(feet) * 12
+    print(str(inches) + " this is how many inches are in " + feet + " feet")
+
+inputFeet = input("how many feet are there? ")
+FeetToInches(inputFeet)
