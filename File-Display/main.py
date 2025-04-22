73dd528d93338111b06f9ef4d7c1ba131886b6e7
Temple <127430671+Tdavis23@users.noreply.github.com>
2025-04-22 20:01:37 +0000

    completed file display assignment. kinda took a little different. Hope you like it :)

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,22 @@
 #
-# Name
-# Date
+# Trenton B. Davis
+# 04/22/25
 # File Display Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+filePath = 'File-Display/numbers.txt'
+
+def listFile():
+    try:
+        with open(filePath, 'r') as file:
+            #read and display the file contents
+            for line in file:
+                print(line.strip())
+    except FileNotFoundError:
+        print('File does not exist')
+
+
+#call function
+listFile()
